f35c1100cb19be2255f38492fd9701f07c7264da
rf777rf777 <rf777rf777@gmail.com>
2018-07-08 23:07:29 +0800

    add some practice

---
+++ Tensorflow/FirstNN/Build_NN.py
@@ -0,0 +1,49 @@
+import def_add_layer
+import tensorflow as tf 
+import numpy as np
+
+x_data = np.linspace(-1,1,300)[:,np.newaxis]
+
+#噪點 使之更像真實數據
+noise = np.random.normal(0,0.05,x_data.shape)
+
+y_data = np.square(x_data) - 0.5 + noise
+
+#x_data屬性只有1(假設只有1個神經元)
+xs = tf.placeholder(tf.float32,[None,1])
+
+#y_data屬性只有1(假設只有1個神經元)
+ys = tf.placeholder(tf.float32,[None,1])
+'''
+	建立一個3層神經NN:
+	輸入層有1個神經元 
+	隱藏層有10個神經元 
+	輸出層有1個神經元
+'''
+
+#隱藏層 輸入1(輸入層1個神經元) 輸出10(隱藏層要10個神經元)
+#激勵函數嘗試使用relu	
+#layer1 = def_add_layer.add_layer(x_data,1,10,activation_function = tf.nn.relu)
+layer1 = def_add_layer.add_layer(xs,1,10,activation_function = tf.nn.relu)
+
+#輸出層 輸入10(隱藏層要10個神經元) 輸出1(輸出層1個神經元)
+#假設沒有激勵函數	
+#prediction = def_add_layer.add_layer(layer1,10,1,activation_function = None)
+prediction = def_add_layer.add_layer(layer1,10,1,activation_function = None)
+
+#求平均誤差(預測值與真實值的差別)
+loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction),
+	reduction_indices=[1]))
+
+#學習優化器
+#學習效率learning_rate必須小於1 這已假設1
+#每一個train都縮小誤差minimize(loss)
+train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
+
+init = tf.initialize_all_variables()
+with tf.Session() as sess:
+	sess.run(init)
+	for i in range(1000):
+		sess.run(train_step,feed_dict={xs:x_data,ys:y_data})
+		if i % 50:
+			print(sess.run(loss,feed_dict={xs:x_data,ys:y_data}))
--- Tensorflow/FirstNN/def_add_layer.py
+++ Tensorflow/FirstNN/def_add_layer.py
@@ -5,7 +5,7 @@ def add_layer(inputs,in_size,out_size,activation_function=None):
 	#權重
 	Weights = tf.Variable(tf.random_normal([in_size,out_size]))
 	#偏差 (機器學習中建議偏差不要為0 所以我們在這裡加上0.1)
-	biases = tf.Variable(tf.zeros(1,out_size) + 0.1)
+	biases = tf.Variable(tf.zeros([1,out_size]) + 0.1)
 	#matmul：矩陣乘法
 	Wx_plus_b = tf.matmul(inputs,Weights) + biases
 
